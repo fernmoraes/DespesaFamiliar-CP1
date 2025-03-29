fa60486ad3cd92a3af90df42b12cd6c2a5124b14
Fernando Navajas Moraes <100091001+fernmoraes@users.noreply.github.com>
2025-03-29 13:58:43 -0300

    Adicionado Try Catch para o scanner

--- src/br/com/fiap/main/Main.java
+++ src/br/com/fiap/main/Main.java
@@ -12,42 +12,47 @@ import br.com.fiap.bean.DespesaFamiliar;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner;
 
-        // Inicia o Scanner
-        Scanner scanner = new Scanner(System.in);
+        try {
+            // Inicia o Scanner
+            scanner = new Scanner(System.in);
 
-        // Entrada de dados
-        System.out.print("Digite o valor da renda familiar: ");
-        double rendaFamiliar = scanner.nextDouble();
-        System.out.print("Digite o número de moradores: ");
-        int numeroDeMoradores = scanner.nextInt();
-        System.out.print("Digite o gasto com luz: ");
-        double gastoComLuz = scanner.nextDouble();
-        System.out.print("Digite o gasto com água: ");
-        double gastoComAgua = scanner.nextDouble();
-        System.out.print("Digite o gasto com internet: ");
-        double gastoComInternet = scanner.nextDouble();
-        System.out.print("Digite o valor da mensalidade da academia: ");
-        double valorMensalidadeDaAcademia = scanner.nextDouble();
+            // Entrada de dados
+            System.out.print("Digite o valor da renda familiar: ");
+            double rendaFamiliar = scanner.nextDouble();
+            System.out.print("Digite o número de moradores: ");
+            int numeroDeMoradores = scanner.nextInt();
+            System.out.print("Digite o gasto com luz: ");
+            double gastoComLuz = scanner.nextDouble();
+            System.out.print("Digite o gasto com água: ");
+            double gastoComAgua = scanner.nextDouble();
+            System.out.print("Digite o gasto com internet: ");
+            double gastoComInternet = scanner.nextDouble();
+            System.out.print("Digite o valor da mensalidade da academia: ");
+            double valorMensalidadeDaAcademia = scanner.nextDouble();
 
-        // Instância da classe DespesaFamiliar
-        DespesaFamiliar despesa = new DespesaFamiliar();
+            // Instância da classe DespesaFamiliar
+            DespesaFamiliar despesa = new DespesaFamiliar();
 
-        // Dados inseridos salvos
-        despesa.setRendaFamiliar(rendaFamiliar);
-        despesa.setNumeroDeMoradores(numeroDeMoradores);
-        despesa.setGastoComLuz(gastoComLuz);
-        despesa.setGastoComAgua(gastoComAgua);
-        despesa.setGastoComInternet(gastoComInternet);
-        despesa.setValorMensalidadeDaAcademia(valorMensalidadeDaAcademia);
+            // Dados inseridos salvos
+            despesa.setRendaFamiliar(rendaFamiliar);
+            despesa.setNumeroDeMoradores(numeroDeMoradores);
+            despesa.setGastoComLuz(gastoComLuz);
+            despesa.setGastoComAgua(gastoComAgua);
+            despesa.setGastoComInternet(gastoComInternet);
+            despesa.setValorMensalidadeDaAcademia(valorMensalidadeDaAcademia);
 
-        // Calcula o total de despesas
-        double totalDespesas = despesa.calcularTotalDeDespesas();
-        double rendaLiquida = despesa.calcularRendaFamiliarLiquida();
+            // Calcula o total de despesas
+            double totalDespesas = despesa.calcularTotalDeDespesas();
+            double rendaLiquida = despesa.calcularRendaFamiliarLiquida();
 
-        // Exibir no terminal
-        System.out.printf("Renda Familiar: %.3f%n", rendaFamiliar);
-        System.out.printf("Total de Despesas: %.3f%n", totalDespesas);
-        System.out.printf("Renda Familiar Líquida: %.3f%n", rendaLiquida);
+            // Exibir no terminal
+            System.out.printf("Renda Familiar: %.3f%n", rendaFamiliar);
+            System.out.printf("Total de Despesas: %.3f%n", totalDespesas);
+            System.out.printf("Renda Familiar Líquida: %.3f%n", rendaLiquida);
+        } catch (Exception e) {
+            System.out.println("Formato Inválido.");
+        }
     }
 }
